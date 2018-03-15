7a38011773478bcaff3408b7ea2730260b7edd37
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-03-15 22:41:49 +0100

    fix moved tests

--- test/store/documentStoreTest.js
+++ test/store/documentStoreTest.js
@@ -17,12 +17,12 @@ describe('document store', () => {
   common(() => store)
 
   it('insert should fail with invalid name', async () => {
-    return store().collection('templates').insert({ name: '<test' }).should.be.rejected()
+    return store.collection('templates').insert({ name: '<test' }).should.be.rejected()
   })
 
   it('update should fail with invalid name', async () => {
-    await store().collection('templates').insert({ name: 'test' })
+    await store.collection('templates').insert({ name: 'test' })
 
-    return store().collection('templates').update({ name: 'test' }, { $set: { name: '/foo/other' } }).should.be.rejected()
+    return store.collection('templates').update({ name: 'test' }, { $set: { name: '/foo/other' } }).should.be.rejected()
   })
 })
