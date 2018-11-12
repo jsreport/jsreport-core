77af0ae8b612a72b23de23176f5db3b042438580
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-11-12 09:46:40 +0100

    fix parsing json with $ref when there is array with primitives https://forum.jsreport.net/topic/813/cannot-use-in-operator-to-search-for-ref-in-0

---
+++ test/render/resolveReferencesTest.js
@@ -0,0 +1,11 @@
+const resolveReferences = require('../../lib/render/resolveReferences')
+require('should')
+
+describe('resolveReferences', () => {
+  it('should work with $ref schema and array with primitive', () => {
+    resolveReferences({
+      '$id': '1',
+      'foo': [1, 2, 3]
+    }).should.be.eql({foo: [1, 2, 3]})
+  })
+})
